b7fcf8aa2ff2baccae05123b03a197a86384ffa4
axhique <124482531+axhique@users.noreply.github.com>
2023-02-05 11:01:08 +0530

    Delete fiboancci.py

--- fiboancci.py
+++
@@ -1,9 +0,0 @@
-first_num = 0
-second_num = 1
-print (first_num ,second_num,end=' ')
-
-for i in range(10):
-    sum = first_num + second_num
-    first_num = second_num
-    second_num = sum
-    print (sum ,end=' ')
